b5ebee84ea201e596147d3b9209d493f458a1af7
Sylvirhm <rahmasylvi09@gmail.com>
2023-07-24 13:41:39 +0700

    detail jasa

---
+++ validation.js
@@ -0,0 +1,20 @@
+function cekEmail(){
+    var inpEmail = document.getElementById('email')
+    var email = inpEmail.value;
+    var arr_email = email.split("@")
+
+    if (arr_email[1] != 'gmail.com' && email != ''){
+        alert('Email yang di gumanakan wajib menggunakan email gmail');
+        inpEmail.value = '';
+    }
+    console.log(email)
+}
+function cekPass(){
+    var inPass = document.getElementById('password');
+    var pass = inPass.value;
+
+    if (pass.length < 8 && pass != ''){
+        alert("password minimal 8 karakter")
+    }
+    console.log(pass)
+}
